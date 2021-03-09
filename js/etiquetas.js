6e6ae2a02b1bd9908ef794935ee0f9fb8c4db12c
CCM SMART TECHNOLOGY <79609478+CCM-SMART@users.noreply.github.com>
2021-03-08 22:22:21 -0500

    Add files via upload

---
+++ js/etiquetas.js
@@ -0,0 +1,58 @@
+let btn = document.querySelector('.btn');
+let card = document.querySelector('.card');
+
+
+btn.addEventListener('click', () =>{
+    card.classList.toggle('active');
+});
+//
+let alexander = document.querySelector('#alexander');
+let cristian = document.querySelector('#cristian');
+
+
+alexander.addEventListener('click', () =>{
+    cristian.classList.toggle('active');
+});
+
+
+//
+let dennis = document.querySelector('#dennis');
+let Javier = document.querySelector('#Javier');
+
+
+dennis.addEventListener('click', () =>{
+    Javier.classList.toggle('active');
+});
+
+
+
+//
+let calderon= document.querySelector('#calderon');
+let robert = document.querySelector('#robert');
+
+
+calderon.addEventListener('click', () =>{
+    robert.classList.toggle('active');
+});
+
+
+
+//
+let Calderon_MOreno = document.querySelector('#Calderon_MOreno');
+let maycol = document.querySelector('#maycol');
+
+
+Calderon_MOreno.addEventListener('click', () =>{
+    maycol.classList.toggle('active');
+});
+//
+
+
+let Calderon_Mo = document.querySelector('#Calderon_Mo');
+let bryan = document.querySelector('#bryan');
+
+
+Calderon_Mo.addEventListener('click', () =>{
+    bryan.classList.toggle('active');
+});
+//
